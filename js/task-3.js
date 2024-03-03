800bdd97de823656cea8ee6283e78453145a32ea
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-03-03 16:18:45 +0200

    task 3

---
+++ js/task-3.js
@@ -0,0 +1,42 @@
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у 
+// приватну властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, 
+// який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value 
+// об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості 
+// value об'єкта, який викликає цей метод.
+
+class StringBuilder {
+constructor(initialValue){
+  this._value = initialValue;
+};
+getValue() {
+  return this._value;
+};
+padEnd(str) {
+  this._value += str;
+};
+padStart(str) {
+  this._value = str + this._value;
+};
+padBoth(str) {
+  this._value = str + this._value + str;
+};
+}
+
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
